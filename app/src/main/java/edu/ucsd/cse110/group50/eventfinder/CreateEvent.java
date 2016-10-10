0d4d9168a6f5728a5c2b68dd049119cb332d6a07
Thiago Marback <tmarback@ucsd.edu>
2016-10-09 22:00:47 -0700

    Finished Create Event page.

--- app/src/main/java/edu/ucsd/cse110/group50/eventfinder/CreateEvent.java
+++ app/src/main/java/edu/ucsd/cse110/group50/eventfinder/CreateEvent.java
@@ -4,9 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
+import org.w3c.dom.Text;
+
+import java.sql.Time;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -33,6 +43,10 @@ public class CreateEvent extends AppCompatActivity {
 
         CardView dateCard = (CardView) findViewById( R.id.dateCard );
         dateCard.setVisibility( View.VISIBLE );
+        ImageView blur = (ImageView) findViewById( R.id.backgroundBlur );
+        blur.setVisibility( View.VISIBLE );
+        setFormClickable( false );
+
         DatePicker datePicker = (DatePicker) findViewById( R.id.datePicker );
         datePicker.init( selectedYear, selectedMonth, selectedDay, new DateChangeListener() );
 
@@ -43,8 +57,6 @@ public class CreateEvent extends AppCompatActivity {
         @Override
         public void onDateChanged( DatePicker datePicker, int year, int month, int day ) {
 
-            CardView dateCard = (CardView) findViewById( R.id.dateCard );
-            dateCard.setVisibility( View.INVISIBLE );
             selectedDay = day;
             selectedMonth = month;
             selectedYear = year;
@@ -58,29 +70,108 @@ public class CreateEvent extends AppCompatActivity {
             }
             String weekday = new SimpleDateFormat("EE").format( selectedDate );
 
-            TextView eventDate = (TextView)findViewById( R.id.eventDate );
-            TextView errorMessage = (TextView)findViewById( R.id.invalidDateMessage );
-
+            TextView eventDate = (TextView) findViewById( R.id.eventDate );
             eventDate.setText( weekday + ", " + date );
-            if ( checkValidDate() ) {
-                eventDate.setTextColor( 0xFF000000 );
-                errorMessage.setVisibility( View.INVISIBLE );
-            } else {
-                eventDate.setTextColor( 0xFFF44336 );
-                errorMessage.setVisibility( View.VISIBLE );
-            }
+            checkValidDate();
+
+            CardView dateCard = (CardView) findViewById( R.id.dateCard );
+            dateCard.setVisibility( View.INVISIBLE );
+            ImageView blur = (ImageView) findViewById( R.id.backgroundBlur );
+            blur.setVisibility( View.INVISIBLE );
+            setFormClickable( true );
 
         }
 
     }
 
-    private boolean checkValidDate() {
+    public void pickTime( View v ) {
 
+        CardView timeCard = (CardView) findViewById( R.id.timeCard );
+        timeCard.setVisibility( View.VISIBLE );
+        ImageView blur = (ImageView) findViewById( R.id.backgroundBlur );
+        blur.setVisibility( View.VISIBLE );
+        setFormClickable( false );
+
+        TimePicker timePicker = (TimePicker) findViewById( R.id.timePicker );
+        timePicker.setHour( selectedHour );
+        timePicker.setMinute( selectedMinute );
+
+    }
+
+    public void setTime( View v ) {
+
+        TimePicker selected = (TimePicker) findViewById( R.id.timePicker );
+        int hour = selected.getHour();
+        int minute = selected.getMinute();
+
+        selectedHour = hour;
+        selectedMinute = minute;
+
+        String time = hour + ":" + minute;
+        TextView eventTime = (TextView) findViewById( R.id.eventTime );
+        eventTime.setText( time );
+        checkValidDate();
+        closeTime( v );
+
+    }
+
+    public void closeTime( View v ) {
+
+        CardView timeCard = (CardView) findViewById( R.id.timeCard );
+        timeCard.setVisibility( View.INVISIBLE );
+        ImageView blur = (ImageView) findViewById( R.id.backgroundBlur );
+        blur.setVisibility( View.INVISIBLE );
+        setFormClickable( true );
+
+    }
+
+    public void createEvent( View v ) {
+
+    }
+
+    private void checkValidDate() {
+
+        TextView eventDate = (TextView) findViewById( R.id.eventDate );
+        TextView eventTime = (TextView) findViewById( R.id.eventTime );
+        TextView errorMessage = (TextView) findViewById( R.id.invalidDateMessage );
         Calendar selected = Calendar.getInstance();
         selected.set( selectedYear, selectedMonth, selectedDay, selectedHour, selectedMinute );
         Calendar current = Calendar.getInstance();
 
-        return !selected.before( current );
+        if ( !selected.before( current ) ) {
+            eventDate.setTextColor( 0xFF000000 );
+            eventTime.setTextColor( 0xFF000000 );
+            errorMessage.setVisibility( View.INVISIBLE );
+        } else {
+            eventDate.setTextColor( 0xFFF44336 );
+            eventTime.setTextColor( 0xFFF44336 );
+            errorMessage.setVisibility( View.VISIBLE );
+        }
 
     }
+
+    private void setFormClickable( boolean clickable ) {
+
+        EditText name = (EditText) findViewById( R.id.eventName );
+        TextView date = (TextView) findViewById( R.id.eventDate ) ;
+        TextView time = (TextView) findViewById( R.id.eventTime ) ;
+        CheckBox passwordToggle = (CheckBox) findViewById( R.id.passOption );
+        EditText password = (EditText) findViewById( R.id.eventPassword );
+        Switch restrictionsToggle = (Switch) findViewById( R.id.restrictionsToggle );
+        EditText restrictions = (EditText) findViewById( R.id.eventRestrictions );
+        EditText description = (EditText) findViewById( R.id.eventDescription );
+        Button finish = (Button) findViewById( R.id.doneButton );
+
+        name.setFocusable( clickable );
+        date.setClickable( clickable );
+        time.setClickable( clickable );
+        passwordToggle.setClickable( clickable );
+        password.setFocusable( clickable );
+        restrictionsToggle.setClickable( clickable );
+        restrictions.setFocusable( clickable );
+        description.setFocusable( clickable );
+        finish.setClickable( clickable );
+
+    }
+
 }
--- app/src/main/java/edu/ucsd/cse110/group50/eventfinder/EventDetailFragment.java
+++ app/src/main/java/edu/ucsd/cse110/group50/eventfinder/EventDetailFragment.java
@@ -61,7 +61,7 @@ public class EventDetailFragment extends Fragment {
 
         // Show the dummy content as text in a TextView.
         if (mItem != null) {
-            ((TextView) rootView.findViewById(R.id.event_detail)).setText(mItem.details);
+            //((TextView) rootView.findViewById(R.id.event_detail)).setText(mItem.details);
         }
 
         return rootView;
