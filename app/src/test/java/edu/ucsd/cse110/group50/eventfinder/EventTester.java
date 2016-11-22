677eff04c332e5435997e103926cd5b243b13975
Thiago Marback <tmarback@ucsd.com>
2016-11-21 18:43:23 -0800

    Merged.

--- app/src/test/java/edu/ucsd/cse110/group50/eventfinder/EventTester.java
+++ app/src/test/java/edu/ucsd/cse110/group50/eventfinder/EventTester.java
@@ -26,11 +26,7 @@ public class EventTester {
 
         event = new Event( "420", "A new world", "1337" );
 
-        event.setHour( (byte) 10 );
-        event.setMinute( (byte) 2 );
-        event.setDay( (byte) 15 );
-        event.setMonth( (byte) 3 );
-        event.setYear( (short) 2015 );
+        event.setDate( new EvDate( 10, 2, 15, 3, 2015 ) );
 
         event.setAddress( "UCSD" );
         event.setHasPassword( true );
@@ -42,91 +38,6 @@ public class EventTester {
         restrictions.add( "Must be able to do dank 720 MLG quickscopes" );
         event.setRestrictions( restrictions );
         event.setDescription( "Come do stuff" );
-
-    }
-
-    /**
-     * Tests if the getTime() method is returning the correct time string.
-     */
-    @Test
-    public void testGetTime() {
-
-        assertEquals( "Wrong time string returned.", "10:02", event.getTime() );
-
-        // Tests padding zeroes.
-        event.setHour( (byte) 0 );
-        event.setMinute( (byte) 0 );
-        assertEquals( "Wrong time string returned.", "00:00", event.getTime() );
-
-    }
-
-    /**
-     * Tests if the getDate() method is returning the correct date string.
-     */
-    @Test
-    public void testGetDate() {
-
-        assertEquals( "Wrong date string returned.", "03/15/2015", event.getDate() );
-
-        // Tests padding zeroes.
-        event.setDay( (byte) 0 );
-        event.setMonth( (byte) 0 );
-        event.setYear( (short) 0 );
-        assertEquals( "Wrong date string returned.", "00/00/0000", event.getDate() );
-
-    }
-
-    /**
-     * Tests if the setTime() (using individual values) method is setting event time correctly.
-     */
-    @Test
-    public void testSetTimeValues() {
-
-        event.setTime( (byte) 2, (byte) 59 );
-
-        assertEquals( "Wrong hour set.", 2, event.getHour() );
-        assertEquals( "Wrong minute set.", 59, event.getMinute() );
-
-    }
-
-    /**
-     * Tests if the setTime() (using a String) method is setting event time correctly.
-     */
-    @Test
-    public void testSetTimeString() {
-
-        event.setTime( "11:45" );
-
-        assertEquals( "Wrong hour set.", 11, event.getHour() );
-        assertEquals( "Wrong minute set.", 45, event.getMinute() );
-
-    }
-
-    /**
-     * Tests if the setDate() (using individual values) method is setting event date correctly.
-     */
-    @Test
-    public void testSetDateValues() {
-
-        event.setDate( (byte) 24, (byte) 12, (short) 2042 );
-
-        assertEquals( "Wrong day set.", 24, event.getDay() );
-        assertEquals( "Wrong month set.", 12, event.getMonth() );
-        assertEquals( "Wrong year set.", 2042, event.getYear() );
-
-    }
-
-    /**
-     * Tests if the setDate() (using a String) method is setting event time correctly.
-     */
-    @Test
-    public void testSetDateString() {
-
-        event.setDate( "10/11/1997" );
-
-        assertEquals( "Wrong day set.", 11, event.getDay() );
-        assertEquals( "Wrong month set.", 10, event.getMonth() );
-        assertEquals( "Wrong year set.", 1997, event.getYear() );
 
     }
 
--- app/src/test/java/edu/ucsd/cse110/group50/eventfinder/UserTester.java
+++ app/src/test/java/edu/ucsd/cse110/group50/eventfinder/UserTester.java
@@ -25,66 +25,6 @@ public class UserTester {
     public void setUp() {
 
         user = new User( "1337", "A new user" );
-        user.addHosted( "42" );
-        user.addHosted( "1000" );
-        user.addHosted( "1" );
-        user.addHosted( "66" );
-
-    }
-
-    /**
-     * Tests if the addHosted() method is adding events correctly.
-     */
-    @Test
-    public void testAddHosted() {
-
-        ArrayList<String> hosted = user.getHostedEvents();
-        assertTrue( "Event not hosted.", hosted.contains( "42" ) );
-        assertTrue( "Event not hosted.", hosted.contains( "1000" ) );
-        assertTrue( "Event not hosted.", hosted.contains( "1" ) );
-        assertTrue( "Event not hosted.", hosted.contains( "66" ) );
-        assertFalse( "Event should not be hosted.", hosted.contains( "999" ) );
-
-    }
-
-    /**
-     * Tests if the removeHosted() method is cancelling events correctly.
-     */
-    @Test
-    public void testRemoveHosted() {
-
-        user.removeHosted( "1000" );
-
-        ArrayList<String> hosted = user.getHostedEvents();
-        assertTrue( "Event not hosted.", hosted.contains( "42" ) );
-        assertTrue( "Event not hosted.", hosted.contains( "1" ) );
-        assertTrue( "Event not hosted.", hosted.contains( "66" ) );
-        assertFalse( "Event should not be hosted.", hosted.contains( "1000" ) );
-
-    }
-
-    /**
-     * Tests if the eventDone() method is moving hosted events to past correctly.
-     */
-    @Test
-    public void testEventDone() {
-
-        user.eventDone( "66" );
-        user.eventDone( "1" );
-
-        // Checks still to be hosted events.
-        ArrayList<String> hosted = user.getHostedEvents();
-        assertTrue( "Event not hosted.", hosted.contains( "42" ) );
-        assertTrue( "Event not hosted.", hosted.contains( "1000" ) );
-        assertFalse( "Event should not be hosted.", hosted.contains( "66" ) );
-        assertFalse( "Event should not be hosted.", hosted.contains( "1" ) );
-
-        // Checks past events.
-        ArrayList<String> past = user.getPastHosted();
-        assertTrue( "Event not past.", past.contains( "66" ) );
-        assertTrue( "Event not past.", past.contains( "1" ) );
-        assertFalse( "Event should not be past.", past.contains( "42" ) );
-        assertFalse( "Event should not be past.", past.contains( "1000" ) );
 
     }
 
